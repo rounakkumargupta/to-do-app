98f997d31d4016067abe42dfc2453aa4813e03ff
Rounak Kumar Gupta <kumarguptarounak7@gmail.com>
2025-10-04 19:49:30 +0530

    first-commit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from "react"
+import './index.css'
+
+function ToDoList() {
+    const [tasks, setTasks] = useState(['Wake Up', 'Do Nothing', 'Sleep']);
+    const [newTask, setNewTask] = useState('');
+    
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+    }
+    function addTask(){
+        if(newTask.trim() !== ''){
+            setTasks(t => [...t, newTask]);
+            setNewTask('');
+        }
+    }
+    function removeTask(index){
+        const updatedTasks = tasks.filter((element, i) => i !== index);
+        setTasks(updatedTasks);
+    }
+    function moveTaskUp(index){
+        if (index > 0) {
+            const uT = [...tasks];
+            [uT[index], uT[index - 1]] = [uT[index - 1], uT[index]];
+            setTasks(uT);
+        }
+    }
+    function moveTaskDown(index){
+        if (index < tasks.length - 1) {
+            const uT = [...tasks];
+            [uT[index], uT[index + 1]] = [uT[index + 1], uT[index]];
+            setTasks(uT);
+        }
+    }
+    return(
+        <div className="to-do-window">
+            <h2>To Do App</h2>
+            <div>
+                <input type="text" placeholder="Enter task here..." value={newTask} onChange={handleInputChange}/>
+                <button className="addBtn" onClick={addTask}>
+                    Add
+                </button>
+            </div>
+
+            <ol>
+                {tasks.map((task, index) => 
+                    <li key={index}>
+                        <span>{task}</span>
+                        <div className="btns">
+                            <button className="delete" onClick={() => removeTask(index)}>Delete</button>
+                            <button className="move" onClick={() => moveTaskUp(index)}>Up</button>
+                            <button className="move" onClick={() => moveTaskDown(index)}>Down</button>
+                        </div>
+                    </li>
+                )}
+            </ol>
+        </div>
+    )
+    
+}
+export default ToDoList
